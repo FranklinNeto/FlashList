25d4a3050404f99f435c0adcf98ffddd6160c67e
Thales Araújo <t.araujods@gmail.com>
2024-05-18 11:13:21 -0400

    organização dos arquivos na arquitetura MVC.

---
+++ challenge-4/View/Forms1.swift
@@ -0,0 +1,18 @@
+//
+//  Forms 1.swift
+//  challenge-4
+//
+//  Created by Thales Araújo on 18/05/24.
+//
+
+import SwiftUI
+
+struct Forms_1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Forms_1()
+}
---
+++ challenge-4/View/Forms4.swift
@@ -0,0 +1,18 @@
+//
+//  Forms 4.swift
+//  challenge-4
+//
+//  Created by Thales Araújo on 18/05/24.
+//
+
+import SwiftUI
+
+struct Forms_4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Forms_4()
+}
