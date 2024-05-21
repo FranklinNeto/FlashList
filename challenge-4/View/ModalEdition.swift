9d99c94974d42b317a0c17e397773d695748a90b
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2024-05-21 15:21:35 -0400

    deletando forms

--- challenge-4/View/ModalEdition.swift
+++ challenge-4/View/ModalEdition.swift
@@ -1,5 +1,5 @@
 //
-//  ModalEdition.swift
+//  Forms 1.swift
 //  challenge-4
 //
 //  Created by Thales Araújo on 18/05/24.
@@ -7,12 +7,82 @@
 
 import SwiftUI
 
-struct ModalEdition: View {
+struct QuestionnaireView: View {
+    @State private var selectedCategory = "Proteínas"
+    @State private var selectedFrequency = "Semanal"
+    @State private var numberOfPeople = 1
+    
+    var categories = ["Proteínas", "Grãos", "Laticínios", "Frutas e Vegetais"]
+    var frequencies = ["Diária", "Semanal", "Mensal"]
+    
     var body: some View {
-        Text("Modal para edicao dos produtos")
+        
+        VStack{
+            Form {
+                Section(header: Text("Preferências")) {
+                    Picker("Categoria preferida", selection: $selectedCategory) {
+                        ForEach(categories, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    Picker("Frequência de compras", selection: $selectedFrequency) {
+                        ForEach(frequencies, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    Stepper(value: $numberOfPeople, in: 1...10) {
+                        Text("Número de pessoas: \(numberOfPeople)")
+                    }
+                }
+                //            NavigationLink(destination: ContentView() {
+                //                Text("Gerar Lista de Compras")
+                //            }
+            }
+            .navigationTitle("Questionário")
+            
+            Text("Primeira Pergunta do Questionário")
+            
+            NavigationLink(destination: Forms_2()){
+                Text("Ir para a Pergunta 2")
+            }
+        }
     }
+    
 }
 
+
+
+
+
+
+
+
+
+//    @State private var quizzInfo: Info?
+//    @State private var questions: [Question] = []
+//
+//    var body: some View {
+//
+//        if let info = quizzInfo {
+//
+//            Text(info.title)
+//
+//        } else {
+//            VStack(spacing: 4){
+//
+//                ProgressView()
+//                Text ("Please, wait")
+//                    .font(.caption2)
+//                    .foregroundColor(.gray)
+//            }
+//        }
+//
+//
+//    }
+//}
+
 #Preview {
-    ModalEdition()
+
+    QuestionnaireView()
+
 }
