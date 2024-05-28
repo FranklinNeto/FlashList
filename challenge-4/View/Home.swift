b6e6352fba5e4c50c3a5be381a20a35c936d33d7
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2024-05-27 21:25:53 -0400

    primeira tab view alinhada

--- challenge-4/View/Home.swift
+++ challenge-4/View/Home.swift
@@ -3,87 +3,113 @@ import SwiftUI
 struct FirstPageView: View {
     var body: some View {
         
-        ZStack{
+//        ZStack{
+//            
+//            Image("telahome1")
+//                .resizable()
+//                .scaledToFit()
+//                .opacity(0.5)
             
-            Image("telahome1")
+            
+            VStack {
+                
+                Text("FlashList")
+                    .font(.system(size: 28, weight: .bold))
+                    .foregroundColor(Color.corDoTituloDaHome)
+                    .padding(.bottom, 5)
+                    .position(x: 200, y: 40)
+                
+               
+                VStack{
+                    Text("Poupe tempo e simplifique suas")
+                    Text("compras!")
+                }
+                    
+                    
+                    
+                    
+                  //  .background(.red)
+                    
+                    .font(.system(size: 22, weight: .bold))
+                    .foregroundColor(Color.corDoTituloDaHome)
+                    .padding(.top, 20)
+                    .position(x: 190, y: -100)
+                    .multilineTextAlignment(.center)
+                    .offset(x: 10, y:-16)
+                   
+                
+                // Spacer().frame(height: 50)
+                
+                
+                Image("comprartempo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 230)
+                    .padding(.bottom, 20)
+                    .position(x: 190, y: -100)
+                
+                
+                Text("O FlashList cria listas de compras personalizadas com base nas suas preferências e necessidades")
+                    .font(.system(size: 20, weight: .regular))
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                    .offset(y:-160)
+                
+                
+                Spacer()
+                
+                
+            }
+            
+      //  }
+    }
+}
+
+struct SecondPageView: View {
+    var body: some View {
+        
+        ZStack{
+            Image("telahome2")
                 .resizable()
                 .scaledToFit()
                 .opacity(0.5)
             
             
-            
             VStack {
-                Text("FewClicks")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.green)
-                    .padding(.top, 50)
+                Spacer()
                 
-                Text("Poupe tempo e simplifique suas compras!")
-                    .font(.headline)
-                    .foregroundColor(Color.green)
-                    .padding(.top, 20)
+                Text("Você também pode obter uma estimativa dos preços dos itens da sua lista, ajudando a planejar suas compras melhor!")
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
                 
                 Spacer()
                 
-                Image("comprartempo") // Nome do asset da primeira imagem
+                Image("tela2")
                     .resizable()
                     .scaledToFit()
                     .frame(height: 200)
                     .padding()
                 
-                Text("O FewClicks cria listas de compras personalizadas com base nas suas preferências e necessidades")
+                Text("Para gerar sua lista, basta responder as próximas perguntas!")
                     .multilineTextAlignment(.center)
                     .padding(.horizontal)
+                
+                NavigationLink(destination: QuestionView(viewModel: QuizViewModel())) {
+                    Text("Iniciar")
+                        .padding()
+                       // .frame(maxWidth: .infinity)
+                        .background(Color.corBotaoAtivado) //
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                }
                 
                 Spacer()
                 
             }
         }
     }
+    
 }
-struct SecondPageView: View {
-    var body: some View {
-        VStack {
-            Spacer()
-            
-            Text("Você também pode obter uma estimativa dos preços dos itens da sua lista, ajudando a planejar suas compras melhor!")
-                .multilineTextAlignment(.center)
-                .padding(.horizontal)
-            
-            Spacer()
-            
-            Image("tela2") // Nome do asset da segunda imagem
-                .resizable()
-                .scaledToFit()
-                .frame(height: 200)
-                .padding()
-            
-            Text("Para gerar sua lista, basta responder as próximas perguntas!")
-                .multilineTextAlignment(.center)
-                .padding(.horizontal)
-            
-            NavigationLink(destination: QuestionView(viewModel: QuizViewModel())) {
-                            Text("Iniciar")
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(Color.green)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                                .padding(.horizontal)
-                        }
-            
-            Spacer()
-            
-//            HStack {
-//                Spacer()
-//                PageControlIndicator(numberOfPages: 2, currentPage: 1)
-//                Spacer()
-//            }
-//            .padding(.bottom, 50)
-        }
-    }
-}
-
-
 
